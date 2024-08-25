e8a198f83927fdd4dcfbaab7e1c616c18d565395
Ajay263 <junioralexio607@gmail.com>
2024-08-25 17:53:52 +0200

    Updated the database configs

--- Pakrun/settings.py
+++ Pakrun/settings.py
@@ -83,16 +83,16 @@ WSGI_APPLICATION = 'Pakrun.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
     
 
