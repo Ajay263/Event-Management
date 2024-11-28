2dcd3bb81e058972927490f02367598e363c8edb
Alexio Junior Aaron <97569992+Ajay263@users.noreply.github.com>
2024-11-28 12:03:46 +0200

    Update settings.py

--- Pakrun/settings.py
+++ Pakrun/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!x7)v_!)*c3$f)s-x867_#q74dvjq*(dwvh8rw*4gwcp6)$kw6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["13.53.207.136"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -126,7 +126,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
