00e4e615deda35679848a2acc960e92443263f5e
Alexio Junior Aaron <97569992+Ajay263@users.noreply.github.com>
2024-11-29 14:41:32 +0200

    Update settings.py

--- Pakrun/settings.py
+++ Pakrun/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Events/static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 MEDIA_URL = '/media/'
